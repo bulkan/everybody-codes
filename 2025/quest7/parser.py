4b0a8cdcfbb874de2c9c1e8d457c7c1a6d3acd71
Bulkan Evcimen <bulkan@gmail.com>
2025-11-20 22:53:05 +1100

    [q7]: create a dataclass where rules are put into dict

--- 2025/quest7/parser.py
+++ 2025/quest7/parser.py
@@ -1,5 +1,11 @@
+from dataclasses import dataclass
 from parsita import ParserContext, repsep, reg
-from pprint import pp
+
+
+@dataclass(frozen=True)
+class Notes:
+    names: list[str]
+    rules: dict[str, set[str]]
 
 
 class NotesParsers(ParserContext, whitespace=r"[ ]*"):
@@ -11,7 +17,9 @@ class NotesParsers(ParserContext, whitespace=r"[ ]*"):
     values = names << "\n" << "\n" & rules
 
 
-def parse(input: str):
-    names = NotesParsers.values.parse(input).unwrap()
+def parse(input: str) -> Notes:
+    names, rules_list = NotesParsers.values.parse(input).unwrap()
 
-    pp(names)
+    rules = {left: set(after) for left, after in rules_list}
+
+    return Notes(names=names, rules=rules)
